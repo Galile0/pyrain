aeec5f2b276337b119cdd16a91f8d8d26e34235e
Jan <jan.temme@gmail.com>
2015-12-10 17:04:59 +0100

    Slight restructure, test code if invoked as main module, rotation parsing, alternative vector parsing method as proposed by ZorMonkey in the official thread

--- netstream_parser.py
+++ netstream_parser.py
@@ -3,13 +3,28 @@ from time import time
 import pprint
 
 BOOL = 'bool'
+BV_SIZE = 4   # how many bits represent the component length of ball position vectors
+CV_SIZE = 10  # Same as BV_Size but for Cars respectively
+DV_SIZE = 5   # Default value for non car non ball types
+'''
+NOTES ON VECTOR CODING:
+    According to ZoRMonkeys findings the Car Position Vector consists of 10 Bits for component length, which for frame 0
+     results in 12 bit Component Size
+    According to JJBOts Gihubcode (read_variable_vector) the Car vector for frame 0 has 4 bits for length resulting in
+     14 bit component size
 
+    At this point it is unsure which parsing is correct, but the result of 10 bit for length seem more logical compared
+    to the ball vector (One Axis identical = Lined up with ball, one axis slightly offset, maybe due to mid air spawn,
+    one axis drastical offset, car in front of goal)
+
+    unil further parsing results in clearer data both methods are left in, and can simply be used interchangeable
+'''
 class NetstreamParser:
 
     def __init__(self, frame_number, netstream, objects):
         self.frame_number = frame_number
-        self.netstream = self._reverse_bytewise(netstream)
-        self.objects = objects
+        if netstream: self.netstream = self._reverse_bytewise(netstream)
+        if objects: self.objects = objects
 
     def parse_frames(self):  # Lets try to parse one frame successful before this gets looped
         netstream = self.netstream  # because writing fucking self again and again is annoying as shit
@@ -23,6 +38,7 @@ class NetstreamParser:
         actors = {}
         while True:  # Actor Replicating Loop
             actor = {}
+            actor['start_pos'] = netstream.pos
             actor_present = netstream.read(BOOL)
             if not actor_present:
                 break
@@ -33,17 +49,30 @@ class NetstreamParser:
                 actors[actor_id] = actor
                 break
             actor['actor_data'] = self._parse_new_actor(netstream)
-            actor['vector'] = self._read_variable_vector(netstream)
             actors[actor_id] = actor
         return actors
 
     def _parse_new_actor(self, netstream):
-        unknown = netstream.read(BOOL)
-        type_id = self._reverse_bytewise(netstream.read('bits:32')).uintle
-        type_name = self.objects[type_id]
-        return {'Unknown Flag': unknown,
-                'type_id': type_id,
-                'type_name': type_name}
+        actor = {}
+        actor['unknown_flag'] = netstream.read(BOOL)
+        actor['type_id'] = self._reverse_bytewise(netstream.read('bits:32')).uintle
+        actor['type_name'] = self.objects[actor['type_id']]
+        if 'TheWorld' in actor['type_name']:  # World types are Vector Less
+            return actor
+        if 'Ball_Default' in actor['type_name']:
+            # print("case 1", actor['type_name'])
+            actor['vector'] = self._read_pos_vector(netstream, BV_SIZE)
+            actor['rotation'] = self._read_rot_vector(netstream)
+        elif 'Car_Default' in actor['type_name']:
+            # print("case 2", actor['type_name'])
+            actor['vector'] = self._read_variable_vector(netstream)  # TODO Refer TO HEADERNOTE
+            # actor['vector'] = self._read_pos_vector(netstream, CV_SIZE, False)
+            actor['rotation'] = self._read_rot_vector(netstream)
+
+        else:
+            # print("case 3", actor['type_name'])
+            actor['vector'] = self._read_pos_vector(netstream)
+        return actor
 
     def _reverse_bytewise(self, bitstream, dbg=False):
         # start = time()
@@ -63,28 +92,65 @@ class NetstreamParser:
         x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4)
         return x
 
-    def _read_vector(self, netstream, size):
-        length = self._reverse_bytewise(netstream.read(size)).uintle + 2
-        x = self._reverse_bytewise(netstream.read(length)).uintle
-        y = self._reverse_bytewise(netstream.read(length)).uintle
-        z = self._reverse_bytewise(netstream.read(length)).uintle
-        return x, y, z
-
     def _read_serialized_int(self, netstream, max_value=19):
         value = 0
         bits_read = 0
 
         while True:
+            # print(value, end=' ')
             if netstream.read(1).bool:
                 value += (1 << bits_read)
+            # print(value, end=' ')
             bits_read += 1
+            # print(bits_read, end=' ')
             possible_value = value + (1 << bits_read)
+            # print(possible_value)
             if possible_value > max_value:
+                # print("length", value)
                 return value
 
     def _read_variable_vector(self, netstream):
+        # pos_start = netstream.pos
         length = self._read_serialized_int(netstream) + 2
+        # l_bits = netstream.pos - pos_start
+        # print("Length %d coded in %d bits" % (length, l_bits))
+
         x = self._reverse_bytewise(netstream.read(length)).uintle
         y = self._reverse_bytewise(netstream.read(length)).uintle
         z = self._reverse_bytewise(netstream.read(length)).uintle
+        # netstream.pos = pos
         return (x, y, z)
+
+    def _read_pos_vector(self, netstream, size=DV_SIZE, add=True):
+        # start = netstream.pos
+        length = self._reverse_bytewise(netstream.read(size)).uintle
+        if add: length += 2
+        x = self._reverse_bytewise(netstream.read(length)).uintle
+        y = self._reverse_bytewise(netstream.read(length)).uintle
+        z = self._reverse_bytewise(netstream.read(length)).uintle
+        # delta = netstream.pos - start
+        # netstream.pos=start
+        # print("RAW: %s" % netstream.read(delta).bin)
+        # print("Len: %d" % length)
+        # print("Vec: %d %d %d" % (x,y,z))
+        # print("=======")
+        return x, y, z
+
+    def _read_rot_vector(self, netstream):
+        x = y = z = 0
+        if netstream.read(BOOL):
+            x = self._reverse_byte(netstream.read('uint:8'))
+        if netstream.read(BOOL):
+            y = self._reverse_byte(netstream.read('uint:8'))
+        if netstream.read(BOOL):
+            z = self._reverse_byte(netstream.read('uint:8'))
+        return x, y, z
+
+if __name__=='__main__':
+    v1 = bitstring.ConstBitStream('0b0011000000000000010')
+    v2 = bitstring.ConstBitStream('0x601017b')
+    p = NetstreamParser(0,None,None)
+    r1 = p._read_variable_vector(v1)
+    r2 = p._read_pos_vector(v2)
+    print(r1)
+    print(r2)
