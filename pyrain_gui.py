b2c5638070f3ea3be1abd3c33808312612234b5a
Jan Temme <Jan.Temme@gmail.com>
2015-12-21 04:12:46 +0100

    added experimental flowlayout from pyqt examples. Resizing does not work with this so far (it just applies maximum size)

--- pyrain_gui.py
+++ pyrain_gui.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 from io import StringIO
 
 import math
-from PyQt5.QtCore import QSize, Qt, QRect, QThread, pyqtSignal
+from PyQt5.QtCore import QSize, Qt, QRect, QThread, pyqtSignal, QPoint
 import sys
 from os import path
 from queue import Queue
@@ -150,7 +150,7 @@ class PyRainGui(QMainWindow):
                                   QSizePolicy.MinimumExpanding)
         mpl_content.setSizePolicy(size_policy)
 
-        self.mpl_l = QVBoxLayout(mpl_content)
+        self.mpl_l = FlowLayout(mpl_content)
         self.mpl_l.setSizeConstraint(QLayout.SetMinimumSize)
 
         mpl_scrollarea.setWidget(mpl_content)
@@ -517,6 +517,81 @@ class ThreadedImport(QThread):
 
     def setstop(self):
         self.stop.set()
+
+class FlowLayout(QLayout):
+    def __init__(self, parent=None, margin=0, spacing=-1):
+        super(FlowLayout, self).__init__(parent)
+        if parent is not None:
+            self.setContentsMargins(margin, margin, margin, margin)
+        self.setSpacing(spacing)
+        self.itemList = []
+
+    def __del__(self):
+        item = self.takeAt(0)
+        while item:
+            item = self.takeAt(0)
+
+    def addItem(self, item):
+        self.itemList.append(item)
+
+    def count(self):
+        return len(self.itemList)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self.itemList):
+            return self.itemList[index]
+        return None
+
+    def takeAt(self, index):
+        if 0 <= index < len(self.itemList):
+            return self.itemList.pop(index)
+
+        return None
+
+    def expandingDirections(self):
+        return Qt.Orientations(Qt.Orientation(0))
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        height = self.doLayout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize()
+        for item in self.itemList:
+            size = size.expandedTo(item.minimumSize())
+        margin, _, _, _ = self.getContentsMargins()
+        size += QSize(2 * margin, 2 * margin)
+        return size
+
+    def doLayout(self, rect, testOnly):
+        x = rect.x()
+        y = rect.y()
+        lineHeight = 0
+        for item in self.itemList:
+            wid = item.widget()
+            spaceX = self.spacing() + wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+            spaceY = self.spacing() + wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+            nextX = x + item.sizeHint().width() + spaceX
+            if nextX - spaceX > rect.right() and lineHeight > 0:
+                x = rect.x()
+                y = y + lineHeight + spaceY
+                nextX = x + item.sizeHint().width() + spaceX
+                lineHeight = 0
+            if not testOnly:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+            x = nextX
+            lineHeight = max(lineHeight, item.sizeHint().height())
+        return y + lineHeight - rect.y()
 
 
 def excepthook(excType, excValue, tracebackobj):
