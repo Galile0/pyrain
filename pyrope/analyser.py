413ed95f18245b46dee0bc069052d4b1d551ede7
Jan Temme <Jan.Temme@gmail.com>
2015-12-24 02:41:03 +0100

    Added fix to filter out rogue players (However they happen to appear in the netstream) Added support for Players who appear more than once (join/leave/join) - But thats untested so keep in mind, shit's gonna break yo

--- pyrope/analyser.py
+++ pyrope/analyser.py
@@ -5,70 +5,82 @@ class Analyser:
             raise TypeError("Replay has to be decoded")
         self.replay = replay
         self.player = self._get_player()
+        print(self.player)
 
     def _get_player(self):  # Todo add check that frames are actually parsed
         player = {}
         for frame in self.replay.netstream.values():
             for name, value in frame.actors.items():
-                if "e_Default__PRI_TA" in name:
+                if "e_Default__PRI_TA" in name and "TAGame.PRI_TA:ClientLoadout" in value['data']:
                     try:
-                        player[value['data']['Engine.PlayerReplicationInfo:PlayerName']] = value['actor_id']
+                        playername = value['data']['Engine.PlayerReplicationInfo:PlayerName']
+                        actorid = value['actor_id']
+                        if playername in player:
+                            if actorid not in player[playername]:
+                                player[playername].append(value['actor_id'])
+                        else:
+                            player[playername] = [value['actor_id']]
                     except KeyError:
                         pass
         return player
 
     def get_player_pos(self, player, sep=False):
-        playerid = self.player[player]
-        current_car = -1
-        car_actors = []
-        frame_left = max(self.replay.netstream, key=int)  # Assume player left never, or after last frame
-        player_spawned = False
-        frame_entered = 0
-        for i, frame in self.replay.netstream.items():
-            found_pos = False
-            for actor in frame.actors.values():
-                try:
-                    if actor['data']["Engine.Pawn:PlayerReplicationInfo"][1] == playerid:
-                        current_car = actor['actor_id']
-                except KeyError:
-                    pass
-                if actor['actor_id'] == current_car:
+        playerids = self.player[player]
+        result = []
+        for playerid in playerids:
+            current_car = -1
+            car_actors = []
+            frame_left = max(self.replay.netstream, key=int)
+            player_spawned = False
+            frame_entered = 0
+            for i, frame in self.replay.netstream.items():
+                found_pos = False
+                for actor in frame.actors.values():
                     try:
-                        pos = actor['data']['TAGame.RBActor_TA:ReplicatedRBState']['pos']
-                        car_actors.append(pos)
-                        found_pos = True
-                        if not player_spawned:
-                            player_spawned = True
-                            frame_entered = i
+                        if actor['data']["Engine.Pawn:PlayerReplicationInfo"][1] == playerid:
+                            current_car = actor['actor_id']
                     except KeyError:
                         pass
-            if not found_pos and player_spawned:
-                car_actors.append(car_actors[-1])
-            try:
-                if frame.actors[str(playerid) + 'e_Default__PRI_TA']\
-                        ['data']['Engine.PlayerReplicationInfo:Team'][1] == -1:
-                    # Player got assigned team -1 that means he left the game early
-                    frame_left = i
-                    break
-            except KeyError:
-                pass
-        result = []
-        if sep:
-            slice_frames = [v['frame'] - frame_entered for v in self.replay.header['Goals'] if
-                            frame_entered <= v['frame'] <= frame_left]
-            slice_frames.append(frame_left - frame_entered)
-            lastframe = 0
-            for framenum in slice_frames:
+                    if actor['actor_id'] == current_car:
+                        try:
+                            pos = actor['data']['TAGame.RBActor_TA:ReplicatedRBState']['pos']
+                            car_actors.append(pos)
+                            found_pos = True
+                            if not player_spawned:
+                                player_spawned = True
+                                frame_entered = i
+                        except KeyError:
+                            pass
+                if not found_pos and player_spawned:
+                    car_actors.append(car_actors[-1])
+                try:
+                    if frame.actors[str(playerid) + 'e_Default__PRI_TA']\
+                            ['data']['Engine.PlayerReplicationInfo:Team'][1] == -1:
+                        # Player got assigned team -1 that means he left the game early
+                        frame_left = i
+                        break
+                except KeyError:
+                    pass
+            print('ID', playerid)
+            print('entered', frame_entered)
+            print('left', frame_left)
+            if sep:
+                slice_frames = [v['frame'] - frame_entered for v in self.replay.header['Goals'] if
+                                frame_entered <= v['frame'] <= frame_left]
+                slice_frames.append(frame_left - frame_entered)
+                lastframe = 0
+                for framenum in slice_frames:
+                    result.append({'player': player,
+                                   'start': self.replay.netstream[lastframe].current,
+                                   'end': self.replay.netstream[framenum].current,
+                                   'data': car_actors[lastframe:framenum]})
+                    lastframe = framenum
+            else:
                 result.append({'player': player,
-                               'start': self.replay.netstream[lastframe].current,
-                               'end': self.replay.netstream[framenum].current,
-                               'data': car_actors[lastframe:framenum]})
-                lastframe = framenum
-        else:
-            result.append({'player': player,
-                           'start': self.replay.netstream[frame_entered].current,
-                           'end': self.replay.netstream[frame_left].current,
-                           'data': car_actors[:-1]})
+                               'start': self.replay.netstream[frame_entered].current,
+                               'end': self.replay.netstream[frame_left].current,
+                               'data': car_actors[:-1]})
+        print(result)
         return result
 
     def get_ball_pos(self):
