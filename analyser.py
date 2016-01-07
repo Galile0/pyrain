0679af21f3000b1df5d4fccce6107298d6a4175b
Jan Temme <Jan.Temme@gmail.com>
2016-01-07 11:52:17 +0100

    changed player datastructure. Now included is a join, left and teaminformation for each actor assigned to a playername. This should also resolve issues when leaving team a and rejoining team b

--- analyser.py
+++ analyser.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pprint
+from collections import OrderedDict
 
 
 class Analyser:
@@ -7,7 +7,7 @@ class Analyser:
         if not replay.netstream:
             raise TypeError("Replay has to be decoded")
         self.replay = replay
-        self.player_data = self._get_player()
+        self.player_data = OrderedDict(sorted(self._get_player().items()))
 
     def get_actor_pos(self, name, sep=False):
         if name == 'Ball':
@@ -17,55 +17,60 @@ class Analyser:
         return pos
 
     def _get_player(self):
-        player = {}
-        for frame in self.replay.netstream.values():
-            for name, value in frame.actors.items():
-                if ("e_Default__PRI_TA" in name and
-                        "Engine.PlayerReplicationInfo:Team" in value['data']):
-                    try:
-                        playername = value['data']['Engine.PlayerReplicationInfo:PlayerName']
-                        actorid = value['actor_id']
-                        if playername in player:
-                            if actorid not in player[playername]['cars']:
-                                player[playername]['cars'].append(value['actor_id'])
-                        else:  # This will break if player of team a leaves and rejoins team b
-                            teamid = value['data']['Engine.PlayerReplicationInfo:Team'][1]
-                            player[playername] = {'id': [value['actor_id']],
-                                                  'team': teamid}
-                    except KeyError:
-                        pass
-        team_ids = [v['team'] for k, v in player.items()]
-        id_max = max(team_ids)
-        id_min = min(team_ids)
-        for k, v in player.items():  # Normalize Team Ids to 0 and 1
-            if v['team'] == id_min:
-                v['team'] = 0
-            elif v['team'] == id_max:
-                v['team'] = 1
-            else:
-                raise ValueError("THREE TEAMS?! NOT POSSIBLE!")
-        return player
+        players = {}
+        maxframe = max(self.replay.netstream, key=int)
+        for i, frame in self.replay.netstream.items():
+            pri_ta = [value for name, value in frame.actors.items() if 'e_Default__PRI_TA' in name]
+            for value in pri_ta:
+                teamid = None
+                actorid = value['actor_id']
+                try:
+                    teamid = value['data']['Engine.PlayerReplicationInfo:Team'][1]
+                except KeyError: pass
+                try:
+                    playername = value['data']['Engine.PlayerReplicationInfo:PlayerName']
+                    if playername in players:  # Player already exists
+                        if not any(actorid in data[playername]['id']
+                                   for data in players[playername]):
+                            players[playername].append({'id': actorid,
+                                                        'join': i,
+                                                        'left': maxframe,
+                                                        'team': teamid})
+                    elif 'TAGame.PRI_TA:ClientLoadout' in value['data']:
+                        players[playername] = [{'id': actorid,
+                                                'join': i,
+                                                'left': maxframe,
+                                                'team': teamid}]
+                except KeyError:
+                    pass
+                if teamid == -1:
+                    for actors in players.values():
+                        for actor in actors:
+                            if actorid == actor['id']:
+                                actor['left'] = i
+        return players
 
     def _get_player_pos(self, player, sep=False):
-        playerids = self.player_data[player]['id']
+        actors = self.player_data[player]
         result = []
-        for playerid in playerids:
+        for actor in actors:
             current_car = -1
             car_actors = []
             frame_left = max(self.replay.netstream, key=int)
             player_spawned = False
             frame_entered = 0
-            for i, frame in self.replay.netstream.items():
+            for i in range(actor['join'], actor['left']+1):
+                frame = self.replay.netstream[i]
                 found_pos = False
-                for actor in frame.actors.values():
+                for f_actor in frame.actors.values():
                     try:
-                        if actor['data']["Engine.Pawn:PlayerReplicationInfo"][1] == playerid:
-                            current_car = actor['actor_id']
+                        if f_actor['data']['Engine.Pawn:PlayerReplicationInfo'][1] == actor['id']:
+                            current_car = f_actor['actor_id']
                     except KeyError:
                         pass
-                    if actor['actor_id'] == current_car:
+                    if f_actor['actor_id'] == current_car:
                         try:
-                            pos = actor['data']['TAGame.RBActor_TA:ReplicatedRBState']['pos']
+                            pos = f_actor['data']['TAGame.RBActor_TA:ReplicatedRBState']['pos']
                             car_actors.append(pos)
                             found_pos = True
                             if not player_spawned:
@@ -76,7 +81,7 @@ class Analyser:
                 if not found_pos and player_spawned:
                     car_actors.append(car_actors[-1])
                 try:
-                    actor_data = frame.actors[str(playerid) + 'e_Default__PRI_TA']['data']
+                    actor_data = frame.actors[str(actor['id']) + 'e_Default__PRI_TA']['data']
                     if actor_data['Engine.PlayerReplicationInfo:Team'][1] == -1:
                         # Player got assigned team -1 that means he left the game early
                         frame_left = i
@@ -132,7 +137,7 @@ class Analyser:
             data_p = self.get_ball_pos()
         else:
             data_p = self.get_player_pos(player)
-        vec_p = np.array(data_p[0]['data'])
+        vec_p = np.array(data_p[0]['data'])  # TODO that only views single Player Ids (no rejoin?)
         if reference:
             if reference == 'Ball':
                 data_r = self.get_ball_pos()
@@ -183,6 +188,6 @@ class AnalyserUtils:
             if z:
                 z_coords = [z for x, y, z in coord['data'] if z > 0]
                 result[-1]['z'] = z_coords
-            if not len(x) == len(y):
+            if not len(x_coords) == len(y_coords):
                 raise ValueError('Wrong Dimensions')
         return result
